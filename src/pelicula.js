89a16a9ae04f67d33490faa86af1332d910572e7
Santiago Ramirez <santirlm05@gmail.com>
2026-03-26 11:39:58 +0100

    Mejora estilos y añade animaciones básicas

--- src/pelicula.js
+++ src/pelicula.js
@@ -2,3 +2,49 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
+
+// Animación inicial
+gsap.from(".pelicula-img", {
+    x: -100,
+    opacity: 0,
+    duration: 1,
+    ease: "power2.out",
+});
+
+gsap.from(".pelicula-info h1", {
+    x: 100,
+    opacity: 0,
+    duration: 1,
+    ease: "power2.out",
+    delay: 0.2,
+});
+
+gsap.from(".pelicula-info p", {
+    x: 100,
+    opacity: 0,
+    duration: 1,
+    ease: "power2.out",
+    delay: 0.4,
+});
+
+gsap.from(".valoracion", {
+    x: 100,
+    opacity: 0,
+    duration: 1,
+    ease: "power2.out",
+    delay: 0.6,
+});
+
+// ScrollTrigger básico para el resto
+gsap.utils.toArray(".ficha-item, .recomendada, .comentario, .actor-item").forEach((el) => {
+    gsap.from(el, {
+        scrollTrigger: {
+            trigger: el,
+            start: "top bottom",
+            end: "top center",
+            scrub: 1,
+        },
+        y: 40,
+        opacity: 0,
+    });
+});
