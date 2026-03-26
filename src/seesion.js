17dea11ecbd61a60c8509c10c3496b77dc9238de
Jorge DR <jorgediazra06@gmail.com>
2026-03-26 19:21:19 +0100

    Gitpages

--- src/seesion.js
+++ src/seesion.js
@@ -5,12 +5,18 @@ gsap.registerPlugin(ScrollTrigger);
 
 gsap.registerPlugin(ScrollTrigger);
 
+// PAGINA ENTERA
+const tl = gsap.timeline();
+
+tl.from("nav", { y: -80, opacity: 0, duration: 0.6, ease: "power3.out" })
+  .from(".date-strip", { opacity: 0, duration: 0.4 }, "-=0.2")
+
 // FECHAS
 document.addEventListener("DOMContentLoaded", () => {
   gsap.from(".date-btn", {
     y: 40,
     opacity: 0,
-    duration: 0.1,
+    duration: 0.4,
     ease: "power2.out",
     stagger: 0.10,
     clearProps: "all"
@@ -22,8 +28,7 @@ document.querySelectorAll(".movie-info").forEach((card, i) => {
   gsap.from(card, {
     scrollTrigger: {
       trigger: card,
-      start: "top 90%",
-      toggleActions: "play reverse play reverse"
+      start: "top 90%"
     },
     x: +100,
     opacity: 0,
@@ -35,14 +40,6 @@ document.querySelectorAll(".movie-info").forEach((card, i) => {
 
 // SESIONES POSTER
 document.querySelectorAll(".movie-poster").forEach(poster => {
-  const tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: poster,
-      start: "top 60%",
-      toggleActions: "play reverse play reverse"
-    }
-  });
-
   tl.from(poster, {
     clipPath: "inset(0 100% 0 0)",
     duration: 0.8,
@@ -53,20 +50,4 @@ document.querySelectorAll(".movie-poster").forEach(poster => {
     duration: 1,
     ease: "power2.out"
   }, "<");
-});
-
-// SESIONES TITULO
-document.querySelectorAll(".movie-title").forEach(title => {
-  title.innerHTML = title.textContent
-    .split("")
-    .map(char => `<span>${char === " " ? "&nbsp;" : char}</span>`)
-    .join("");
-
-  gsap.from(title.querySelectorAll("span"), {
-    opacity: 0,
-    y: 20,
-    duration: 0.3,
-    stagger: 0.03,
-    ease: "power2.out"
-  });
 });
