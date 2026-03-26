e0a33599daeec5f0a275a53f64d1e4399368b568
Jorge DR <jorgediazra06@gmail.com>
2026-03-26 20:17:36 +0100

    ultimate fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,22 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/Proyecto_1_TherianusMulticines/',
-  plugins: [
-    tailwindcss(),
-  ],
+    base: '/Proyecto_1_TherianusMulticines/',
+    plugins: [
+        tailwindcss(),
+    ],
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                clubDeLaLucha: resolve(__dirname, 'peliculas/club-de-la-lucha.html'),
+                karate: resolve(__dirname, 'peliculas/karate-a-muerte-en-torremolinos.html'),
+                torrente: resolve(__dirname, 'peliculas/torrente-presidente.html'),
+                cars: resolve(__dirname, 'peliculas/cars.html'),
+                sesiones: resolve(__dirname, 'sesiones/sesiones.html'),
+            }
+        }
+    }
 })
